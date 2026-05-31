4a6d0abb9e02e89d9eab4f015c28a0024284a43c
suneetaramanathan <suneetaramanathan1218@gmail.com>
2026-05-31 02:00:17 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return open("trustnest_website.html").read()
+
+@app.route("/signup")
+def signup():
+    return open("trustnest_signup.html").read()
+
+@app.route("/login")
+def login():
+    return open("trustnest_login.html").read()
+
+@app.route("/contact")
+def contact():
+    return open("trustnest_contact.html").read()
+
+@app.route("/authenticate", methods=["GET", "POST"])
+def authenticate():
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    return open("trustnest_dashboard.html").read()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
